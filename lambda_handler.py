8dc25df5374da05dc587ad1d29e862a6e16d6345
Shaik Sabiha <sabiha.shaik@rampgroup.com>
2025-07-29 13:15:11 +0530

    first commit

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,5 +1,6 @@
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": "Hello from Lambda!"
+        "body": "Hello from Lambda! this is me!"
     }
+                   
